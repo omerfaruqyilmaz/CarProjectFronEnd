b3608758d4ec38a8cf58d594a93205dfd51ee2b6
omerfaruqyilmaz <omerfaruqyilmaz@gmail.com>
2022-01-23 01:20:04 +0300

    CarFrontEnd

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { ColorComponent } from './components/color/color.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { ImageComponent } from './components/image/image.component';
+import { CardetailComponent } from './components/cardetail/cardetail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarComponent,
+    ColorComponent,
+    BrandComponent,
+    NaviComponent,
+    RentalComponent,
+    CustomerComponent,
+    ImageComponent,
+    CardetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/ımage.ts
@@ -0,0 +1,6 @@
+export interface Image{
+    ıd:number;
+    carId:number;
+    ımagePath:string;
+    date:Date;
+}
